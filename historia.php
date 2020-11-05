8617b49982080483ddd6d902df91e67ad47d9c1b
Ewerton Mendonça <netrometro@yahoo.com.br>
2020-11-05 15:30:28 -0300

    Novas páginas

---
+++ historia.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="style.css" />
+    <title>Lodon Café</title>
+  </head>
+  <body class="indigo darken-4">
+    <nav>
+      <div class="nav-wrapper red accent-4">
+        <div class="container">
+          <a href="#" class="brand-logo">
+            <i class="large material-icons">add_alert</i> 
+            London Café
+          </a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="historia.php">História</a></li>
+            <li><a href="menu.php">Menu</a></li>
+            <li><a href="sobre.php">Sobre</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+    
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
---
+++ sobre.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="style.css" />
+    <title>Lodon Café</title>
+  </head>
+  <body class="indigo darken-4">
+    <nav>
+      <div class="nav-wrapper red accent-4">
+        <div class="container">
+          <a href="#" class="brand-logo">
+            <i class="large material-icons">add_alert</i> 
+            London Café
+          </a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="historia.php">História</a></li>
+            <li><a href="menu.php">Menu</a></li>
+            <li><a href="sobre.php">Sobre</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col s4 placa">
+
+        </div>
+        <div class="col s8">
+          <h1>Sobre</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia nunc ut magna aliquet tempor. Integer eget ante suscipit, interdum est sed, vulputate magna. Duis metus metus, accumsan quis lobortis in, facilisis a dui. Donec nulla nunc, ultrices at commodo at, venenatis sodales purus. Maecenas eu mollis ex, vitae facilisis tellus. Pellentesque vitae imperdiet neque, vel bibendum leo. Morbi eget purus libero. Mauris pulvinar, diam sed tincidunt placerat, nibh nulla consequat est, vel vulputate magna eros et orci. Ut quis lobortis ligula, eget dapibus nunc. Proin id bibendum dolor. Morbi ipsum urna, mollis eget velit id, ultricies tincidunt felis. Pellentesque varius ante nec justo suscipit, vel interdum velit blandit. Aenean in mauris vel est lacinia imperdiet et non metus. Sed auctor volutpat est et rutrum. Ut ultricies faucibus semper.</p>
+          <p>Proin vel magna in mauris malesuada mattis a a lorem. Integer vel purus ex. Duis imperdiet consectetur velit, vitae ultricies diam mollis quis. Integer et cursus augue, et rhoncus lorem. Nullam turpis mauris, vulputate id augue eget, lobortis faucibus sem. Nam at elit purus. Morbi iaculis est nec arcu faucibus, a semper lacus pretium. Nullam ullamcorper consequat nisl, a vehicula mi efficitur at. Ut maximus sit amet est tincidunt tempor. Integer eget velit felis. Cras congue sapien orci, sit amet scelerisque risus dignissim et. Cras sollicitudin metus risus. Suspendisse potenti.</p>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
