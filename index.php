3554c7bc2a3634e9ea4d25f0a4cea963c836bc28
Ewerton Mendonça <netrometro@yahoo.com.br>
2020-11-05 14:48:39 -0300

    Design inicial da index

--- index.php
+++ index.php
@@ -4,10 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="style.css" />
     <title>Lodon Café</title>
   </head>
-  <body>
-    <h1>Lodon Café</h1>
+  <body class="blue">
+    <nav>
+      <div class="nav-wrapper red">
+        <div class="container">
+          <a href="#" class="brand-logo">
+            <i class="material-icons">add_alert</i> 
+            Londo Café
+          </a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="historia.php">História</a></li>
+            <li><a href="menu.php">Components</a></li>
+            <li><a href="sobre.php">JavaScript</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+
+    </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
   </body>
